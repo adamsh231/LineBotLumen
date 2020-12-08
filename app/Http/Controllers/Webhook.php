3ad2322770debbcdc120c4242dd6054749b77bd8
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-12-08 08:38:46 +0700

    Initial Commit v17

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -111,7 +111,7 @@ class Webhook extends Controller
     public function getContent($message_id){
         $result = $this->bot->getMessageContent($message_id);
         $this->response->setContent($result->getRawBody());
-        print("<pre>".print_r($this->result,true)."</pre>");
+        print("<pre>".print_r($result,true)."</pre>");
         // return $this->response;
     }
 }
