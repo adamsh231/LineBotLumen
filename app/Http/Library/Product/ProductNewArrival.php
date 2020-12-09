b594bdab3a22986a6f964ca357ac100550383c0c
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-12-09 09:54:29 +0700

    Rework User 7

--- app/Http/Library/Product/ProductNewArrival.php
+++ app/Http/Library/Product/ProductNewArrival.php
@@ -28,23 +28,7 @@ class ProductNewArrival{
     //* --------------------------------------- MODIFIER PUBLIC PROPERTY ----------------------------------------------- *//
 
     public function loadFlex($event){
-        $json = json_decode(file_get_contents(url('flex/new-arrival.json')), true);
-        $product_new_arrival = $this->loadProduct();
-
-        foreach ($product_new_arrival as $key => $value) {
-            $json["contents"][$key] = $json["contents"][0];
-            $json["contents"][$key]["hero"]["url"] = $value["image_url"];
-            $json["contents"][$key]["body"]["contents"][0]["text"] = $value["name"];
-            $json["contents"][$key]["body"]["contents"][1]["contents"][0]["contents"][0]["text"] = $value["brand_name"];
-
-            if ($value['final_price'] != $value['price']) {
-                $json["contents"][$key]["body"]["contents"][2]["text"] = "Rp " . number_format($value["price"], 0, ",", ".");
-                $json["contents"][$key]["body"]["contents"][2]["color"] = "#8c8c8c";
-            }
-
-            $json["contents"][$key]["body"]["contents"][3]["text"] = "Rp " . number_format($value["final_price"], 0, ",", ".");
-            $json["contents"][$key]["footer"]["action"]["uri"] = $this->product->getWebUrlOfficial() . "product/" . $value["id"] . "/0";
-        }
+        $json = $this->flexNewArrival();
 
         $this->httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
             'replyToken' => $event['replyToken'],
@@ -68,6 +52,26 @@ class ProductNewArrival{
         $product_new_arrival = $this->httpClient->get($api_new_arrival);
         $product_new_arrival = json_decode($product_new_arrival->getRawBody(), true);
         return $product_new_arrival;
+    }
+
+    private function flexNewArrival(){
+        $json = json_decode(file_get_contents(url('flex/new-arrival.json')), true);
+        $product_new_arrival = $this->loadProduct();
+        foreach ($product_new_arrival as $key => $value) {
+            $json["contents"][$key] = $json["contents"][0];
+            $json["contents"][$key]["hero"]["url"] = $value["image_url"];
+            $json["contents"][$key]["body"]["contents"][0]["text"] = $value["name"];
+            $json["contents"][$key]["body"]["contents"][1]["contents"][0]["contents"][0]["text"] = $value["brand_name"];
+
+            if ($value['final_price'] != $value['price']) {
+                $json["contents"][$key]["body"]["contents"][2]["text"] = "Rp " . number_format($value["price"], 0, ",", ".");
+                $json["contents"][$key]["body"]["contents"][2]["color"] = "#8c8c8c";
+            }
+
+            $json["contents"][$key]["body"]["contents"][3]["text"] = "Rp " . number_format($value["final_price"], 0, ",", ".");
+            $json["contents"][$key]["footer"]["action"]["uri"] = $this->product->getWebUrlOfficial() . "product/" . $value["id"] . "/0";
+        }
+        return $json;
     }
 
     //* ---------------------------------------------------------------------------------------------------------------- *//
