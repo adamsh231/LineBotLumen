c7baf5a64db32c8ca3bbd4e8b519a53626c3b7ef
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-12-10 07:52:29 +0700

    Message text to JSON V2

--- app/Http/Library/Text.php
+++ app/Http/Library/Text.php
@@ -30,36 +30,20 @@ class Text
         return $this->false_command;
     }
 
-    //* --------------------------------------- MODIFIER PUBLIC PROPERTY ----------------------------------------------- *//
+    //* --------------------------------------- MODIFIER PRIVATE PROPERTY ----------------------------------------------- *//
 
     private function setHelpCommand()
     {
-        // $help = "Command List" . PHP_EOL
-        //     . "'!help' : Melihat list command" . PHP_EOL
-        //     . "'!info' : Informasi Tentang Shoesmart" . PHP_EOL
-        //     . "'!new'  : Menampilkan Produk New Arrival" . PHP_EOL
-        //     . "'!promo': Menampilkan Promo Terbaru";
-        // $this->help_command[0] = array("type" => "text", "text" => $help);
         $this->help_command = json_decode(file_get_contents(url('message/help.json')), true);
     }
 
     private function setFalseCommand()
     {
-        // $false = "Perintah tidak diketahui." . PHP_EOL . "ketik '!help' untuk melihat daftar command";
-        // $this->false_command[0] = array("type" => "text", "text" => $false);
         $this->false_command = json_decode(file_get_contents(url('message/false.json')), true);
     }
 
     private function setInfoCommand()
     {
-        // $intro = "Shoesmart adalah Start up E-commerce khusus sepatu wanita dan pria" . PHP_EOL .
-        //         "yang didirikan pada 2016. Kami hadir sebagai jawaban atas tantangan dunia". PHP_EOL .
-        //         "teknologi yang semakin maju, termasuk dalam hal berbelanja secara online.". PHP_EOL .
-        //         "Sebagai platform online yang mempertemukan antara pebisnis sepatu lokal dan". PHP_EOL .
-        //         "para pembelinya, Shoesmart menyediakan berbagai macam produk sepatu terkini". PHP_EOL .
-        //         "berkualitas dengan harga terjangkau keluaran berbagai brand lokal terbaik di". PHP_EOL .
-        //         "Indonesia.";
-        // $this->info_command[0] = array("type" => "text", "text" => $intro);
         $this->info_command = json_decode(file_get_contents(url('message/info.json')), true);
     }
 
