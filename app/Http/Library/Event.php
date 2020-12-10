c77023d4af09b69b0e9adda242715b1e0b5620e3
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-12-10 14:51:36 +0700

    Event v9

--- app/Http/Library/Event.php
+++ app/Http/Library/Event.php
@@ -20,7 +20,9 @@ class Event
     //* --------------------------------------- MODIFIER PUBLIC PROPERTY ----------------------------------------------- *//
     public function loadTemplate($event)
     {
-        $json = $this->templateEvent();
+        // $json = $this->templateEvent();
+        $json = json_decode(file_get_contents(url('template/event.json')), true);
+
 
         $this->httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
             'replyToken' => $event['replyToken'],
