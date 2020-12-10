14aa978a62200d1fb47f8497012e139e729c686e
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-12-10 18:15:11 +0700

    Event v50

--- app/Http/Library/Event.php
+++ app/Http/Library/Event.php
@@ -56,7 +56,7 @@ class Event
         foreach ($api_event as $key => $value) {
             $json["columns"][$key] = $json["columns"][0];
             $json["columns"][$key]["imageUrl"] = $value["image_square"];
-            $json["columns"][$key]["action"]["label"] = $value["code"];
+            $json["columns"][$key]["action"]["label"] = "xxxx";
             //TODO: Coba ganti label dengan promo!!
         }
         return $json;
