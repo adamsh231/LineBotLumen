1a25b4715d7ed86641760e4fbe44a5af202e3a75
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-12-10 10:45:39 +0700

    Promo List v8

--- app/Http/Library/Promo/PromoList.php
+++ app/Http/Library/Promo/PromoList.php
@@ -22,7 +22,7 @@ class PromoList
     //* --------------------------------------- MODIFIER PUBLIC PROPERTY ----------------------------------------------- *//
     public function loadTemplate($event)
     {
-        $json = json_decode(file_get_contents(url('template/promo-list.json')), true);
+        $json = $this->templatePromoList();
 
         $this->httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
             'replyToken' => $event['replyToken'],
@@ -44,6 +44,21 @@ class PromoList
         $api_promo = $this->httpClient->get($api_promo);
         $api_promo = json_decode($api_promo->getRawBody(), true);
         return $api_promo;
+    }
+
+    private function templatePromoList(){
+        $json = json_decode(file_get_contents(url('template/promo-list.json')), true);
+        $api_promo = $this->loadPromo();
+        foreach ($api_promo as $key => $value) {
+            $json["contents"][$key] = $json["contents"][0];
+            $json["contents"][$key]["hero"]["url"] = $value["image_square"];
+            $json["contents"][$key]["body"]["contents"][0]["text"] = $value["name"];
+            $json["contents"][$key]["body"]["contents"][1]["text"] = $value["description_short"];
+            $json["contents"][$key]["footer"]["contents"][1]["contents"][0]["contents"][1]["text"] = "Coming Soon!";
+            $json["contents"][$key]["footer"]["contents"][1]["contents"][1]["contents"][0]["data"] = "PostBack!";
+            $json["contents"][$key]["footer"]["contents"][1]["contents"][1]["contents"][0]["displayText"] = "Text Post Back";
+        }
+        return $json;
     }
     //* ---------------------------------------------------------------------------------------------------------------- *//
 
