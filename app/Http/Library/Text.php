2e23d63e03fd21ace34ef94048d8c079685800cc
Adam Syarif Hidayatullah <adamsyarif217@yahoo.com>
2020-12-11 08:15:18 +0700

    Reworker info and Select with switch in Webhook

--- app/Http/Library/Text.php
+++ app/Http/Library/Text.php
@@ -4,49 +4,18 @@ namespace App\Http\Library;
 
 class Text
 {
-    private $info_command;
     private $help_command;
     private $false_command;
 
-    public function __construct()
-    {
-        $this->setHelpCommand();
-        $this->setInfoCommand();
-        $this->setFalseCommand();
-    }
-
     public function getHelpCommand()
     {
+        $this->help_command = json_decode(file_get_contents(url('message/help.json')), true);
         return $this->help_command;
-    }
-
-    public function getInfoCommand()
-    {
-        return $this->info_command;
     }
 
     public function getFalseCommand()
     {
+        $this->false_command = json_decode(file_get_contents(url('message/false.json')), true);
         return $this->false_command;
     }
-
-    //* --------------------------------------- MODIFIER PRIVATE PROPERTY ----------------------------------------------- *//
-
-    private function setHelpCommand()
-    {
-        $this->help_command = json_decode(file_get_contents(url('message/help.json')), true);
-    }
-
-    private function setFalseCommand()
-    {
-        $this->false_command = json_decode(file_get_contents(url('message/false.json')), true);
-    }
-
-    private function setInfoCommand()
-    {
-        $this->info_command = json_decode(file_get_contents(url('message/info.json')), true);
-    }
-
-    //* ---------------------------------------------------------------------------------------------------------------- *//
-
 }
